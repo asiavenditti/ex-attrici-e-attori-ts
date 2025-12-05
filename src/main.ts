652f79ea945bd1be9765c695f7f975479a5dac1c
Your Name <you@example.com>
2025-12-05 17:46:25 +0100

    milestone 1 & 2:  Add Person, Nationality, and Actress type definitions to the project

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+
+// Milestone 1
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
+
+
+// Milestone 2
+
+type Nationality =
+  | 'American'
+  | 'British'
+  | 'Australian'
+  | 'Israeli-American'
+  | 'South African'
+  | 'French'
+  | 'Indian'
+  | 'Israeli'
+  | 'Spanish'
+  | 'South Korean'
+  | 'Chinese'
+
+
+
+
+type Actress = Person & {
+
+  most_famous_movies: [string, string, string],
+  awards: string
+  nationality: Nationality
+
+}
+
+// Milestone 3
+
